2e71441071264089301c2fccc710b8756ed1904f
lamentedCauliflower <112721612+lamentedCauliflower@users.noreply.github.com>
2026-01-29 23:35:48 +0000

    add Opentablet Driver

--- hosts/yuro-nixos/hardware-configuration.nix
+++ hosts/yuro-nixos/hardware-configuration.nix
@@ -81,4 +81,7 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  hardware.opentabletdriver.enable = true;
+
 }
